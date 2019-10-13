c941abc271c5c7499f6d4c3f3a0f646889b1563f
pythonredhat <pythonredhat1987@gmail.com>
2019-10-13 10:41:17 -0400

    Add sample python packaging system

---
+++ README.md
@@ -0,0 +1,36 @@
+### Real Python: Python Packaging 
+```bash
+https://realpython.com/pypi-publish-python-package/
+```
+
+### Python Packaging Authority
+```bash
+https://packaging.python.org/guides/distributing-packages-using-setuptools/#distributing-packages
+```
+
+### Build Python Package
+```bash
+python setup.py sdist bdist_wheel
+```
+
+### Run all unit tests in test folder
+```bash
+python -m unittest discover
+```
+
+### Upload Python Package
+```bash
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+```
+
+### Pip Install Package
+```bash
+pip install your-package-name
+pip install hello_world_app
+```
+
+
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = ( HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="hello-world-app",
+    version="1.0.0",
+    description="Hello world app",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pythonredhat/package-skeleton-python",
+    author="Turbo Python",
+    author_email="office@realpython.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+    install_requires=["bs4", "json"],
+    entry_points={
+        "console_scripts": [
+            "hello_world_app=hello_world_app.__main__:main",
+        ]
+    },
+)
